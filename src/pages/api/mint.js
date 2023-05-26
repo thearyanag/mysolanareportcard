72cd4206fef051eb049e5713f15edd9a78a52c37
aryan <agarwalaryan139@gmail.com>
2023-05-27 02:08:13 +0530

    fix: backend url

--- src/pages/api/mint.js
+++ src/pages/api/mint.js
@@ -1,8 +1,6 @@
-import { RawOff } from '@mui/icons-material';
 
 const axios = require('axios');
-
-const searchNFT = require('../../functions/searchNFT');
+const BASE_URL = process.env.BACKEND_URL
 
 export default async function handler(req, res) {
 
@@ -16,60 +14,22 @@ export default async function handler(req, res) {
         const degenData = req.body.data
         const { wallet } = req.body.params;
 
-        console.log("degenData", degenData)
-        console.log("wallet", wallet)
+        console.log(degenData)
+        console.log(wallet)
 
-        let walletData = await searchNFT(wallet);
-
-        if(walletData) {
-            console.log("walletData", walletData)
-            res.status(200).json({ mintAddress: walletData });
-            return;
-        }
-
-
-        if (wallet == undefined) {
-            res.status(400).json({ error: 'Missing receiverAddress' });
-            return;
-        }
-
-
-        let imageId = degenData.status;
-
-        const token = process.env.UNDERDOG_PROTOCOL_API_KEY;
-
-        let url = "";
-        if (imageId == 3) {
-            url = "https://i.imgur.com/oWx6DKY.png"
-        } else if (imageId == 2) {
-            url = "https://i.imgur.com/s2qKwWx.png"
-        } else {
-            url = "https://i.imgur.com/bmFcMj1.png"
-        }
-
-        let data = JSON.stringify({
-            "name": `My Report Card`,
-            "image": url,
-            "receiverAddress": wallet,
-            "attributes": degenData,
-        });
-
-        let config = {
-            method: 'post',
-            url: 'https://api.underdogprotocol.com/v2/projects/c/4/nfts',
-            headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json'
+        const resp = await axios.post(`${BASE_URL}/mint`, {
+            params: {
+                wallet: wallet,
             },
-            data: data
-        };
+            data: {
+                degenData: degenData
+            }
+        })
 
-        const result = axios(config).then(function (response) {
-            return response.data
-        }).catch(function (error) {
-            console.log(error);
-        });
+        console.log(resp.data)
 
-        res.status(200);
+        res.status(200).json("success")
+
+      
     }
 }
